3b76e02f6170a823a74a8359282c6c46353a8cf7
hossai30 <sarah.hossain21@gmail.com>
2019-07-29 20:02:49 -0400

    Testing with f strings

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #Author: Sarah Hossain
 
 import numpy as np
+import matplotlib.pyplot as plt
 dates = [1982, 1980, 1973, 2000]
 
 i = 0
@@ -26,3 +27,15 @@ X=np.array([[1,0],[0,1]])
 Y=np.array([[2,1],[1,2]])
 Z=np.dot(X,Y)
 print(Z)
+
+name = "Sarah"
+print (f'My name is {name}')
+
+years = [1983, 1984, 1985, 1986, 1987]
+total_populations = [8939007, 8954518, 8960387, 8956741, 8943721]
+
+plt.plot(years, total_populations)
+plt.title("Year vs Population in Bulgaria")
+plt.xlabel("Year")
+plt.ylabel("Total Population")
+plt.show()
